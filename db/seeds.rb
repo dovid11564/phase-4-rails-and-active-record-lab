2445d997f487d47c2a6af1ec6830997e4b441bdd
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-12-13 11:59:20 -0500

    commit tree 2e733aaa50dcd20447f56fe46dfd164a127b829b

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: "ur", last_name: "mom", grade: 1)
